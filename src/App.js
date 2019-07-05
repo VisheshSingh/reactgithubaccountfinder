12010b07ea1e3bdf3bc90eae114d848934dd43cc
VisheshSingh <visheshsingh16@gmail.com>
2019-07-04 23:43:55 -0500

    Outputting lists

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
+        <Users />
       </div>
     );
   }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
-  state = {
-    id: '1',
-    login: 'mojombo',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-    html_url: 'https://github.com/mojombo'
-  };
   render() {
-    const { avatar_url, html_url, login } = this.state;
+    const { avatar_url, html_url, login } = this.props.user;
     return (
       <div className="card text-center">
         <img
