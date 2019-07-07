7baa256733a3992e0ccf20ce3f1e1872a9720fc3
VisheshSingh <visheshsingh16@gmail.com>
2019-07-06 21:20:15 -0500

    Home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Alert from './components/layout/Alert';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -19,18 +18,9 @@ const App = props => {
           <div className="App">
             <Navbar />
             <div className="container">
+              <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={props => (
-                    <Fragment>
-                      <Alert />
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />} />
               </Switch>
